bd23e7ce30ae47e583cacb18ff2900a2bb7f698d
Curso <Curso@PORT-Z1.ipartekaula.com>
2019-12-17 10:56:18 +0100

    Eliminado GuardarCargar, metodos implementados en presentacion

--- EjemploAccesoDatos/src/com/ipartek/formacion/ejemploaccesodatos/presentacionconsola/PresentacionConsola.java
+++ EjemploAccesoDatos/src/com/ipartek/formacion/ejemploaccesodatos/presentacionconsola/PresentacionConsola.java
@@ -2,12 +2,13 @@ package com.ipartek.formacion.ejemploaccesodatos.presentacionconsola;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.io.ObjectOutputStream;
 import com.ipartek.formacion.ejemploaccesodatos.accesodatos.Crudable;
-import com.ipartek.formacion.ejemploaccesodatos.accesodatos.GuardarCargar;
 import com.ipartek.formacion.ejemploaccesodatos.accesodatos.PersonaMemoria;
 import com.ipartek.formacion.ejemploaccesodatos.entidades.Persona;
 
@@ -217,7 +218,7 @@ public class PresentacionConsola {
 			case 6:
 				
 				do {
-					GuardarCargar.guardarDatos(ARCHIVO_BBDD);
+					guardarDatos(ARCHIVO_BBDD);
 					System.out.println("Archivo guardado con exito");
 					
 					System.out.println(SALIR_OPCION_ELEGIDA);
@@ -266,6 +267,64 @@ public class PresentacionConsola {
 		
 		}while (opcionMenu != 0);
 		
+	}
+	
+	public static void guardarDatos(String rutaFichero) {
+		Crudable<Persona> dao = PersonaMemoria.getInstancia();
+		FileOutputStream fos = null;
+		ObjectOutputStream oos = null;
+			
+		try {
+			fos = new FileOutputStream(new File(rutaFichero));
+			oos = new ObjectOutputStream(fos);
+				
+			for(Persona persona: dao.getAll()) {
+				//System.out.println(persona);
+				Persona p1 = new Persona(persona.getId(),persona.getNombre(),persona.getApellidos());
+				System.out.println(p1);
+				oos.writeObject(p1);
+			}
+			
+		} catch (FileNotFoundException e1) {
+			throw new RuntimeException("No se ha encontrado el archivo");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			oos.close();
+			fos.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void CargarDatos() {
+		/*Persona p = new Persona();
+		FileInputStream fis = null;
+		ObjectInputStream ois = null;
+		File origen = new File(ARCHIVO_BBDD);
+		PersonaMemoria.class.
+		//File origen = new File("C:\\Users\\curso\\Desktop\\pruebaCSV.txt");
+		 try {
+			 fis = new FileInputStream(origen);
+			 ois = new ObjectInputStream(fis);
+			 try {
+				 for(long i = 0;;i++) {
+					 p = (Persona) ois.readObject();
+					 personas.put(i, p);
+				 }
+			 } catch(EOFException | SocketTimeoutException e) {
+				 //
+			 }
+			 
+		} catch (IOException | ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("Hola");
+			//e1.printStackTrace();
+		}*/
 	}
 
 }
