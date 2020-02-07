dbec1cb93e7763b520aaceeaaa876846c4005234
BorjaBG <Curso@PORT-Z1.ipartekaula.com>
2020-02-07 10:15:35 +0100

    Primer commit

--- WebPersonas/src/com/ipartek/formacion/personas/controlador/loginController.java
+++ WebPersonas/src/com/ipartek/formacion/personas/controlador/loginController.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,6 +16,9 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/login")
 public class loginController extends HttpServlet {
+	private static final String CONTRASEÑA_BBDD = "admin";
+	private static final String USUARIO_BBDD = "root";
+	private static final String URL_BBDD = "jdbc:mysql://localhost:3306/uf2213?serverTimezone=UTC";
 	private static final String LOGIN = "/WEB-INF/vistas/login.jsp";
 	private static final long serialVersionUID = 1L;
 
@@ -24,52 +28,77 @@ public class loginController extends HttpServlet {
 		request.getRequestDispatcher(LOGIN).forward(request, response);
 	}
 
+	@SuppressWarnings("null")
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		
 		String usuario = request.getParameter("usuario");
 		String contraseña = request.getParameter("contraseña");
-		String sqlSelect = "SELECT * FROM usuarios_per WHERE usuario=?";
+		String sqlSelectUsuario = "SELECT usuario FROM usuarios_per";
+		String sqlSelectContraseña = "SELECT contraseña FROM usuarios_per WHERE usuario=?";
 		String us = null;
+		ArrayList<String> arrayUs = new ArrayList<String>();
 		String contra = null;
 		
-		try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/uf2213?serverTimezone=UTC", "root", "admin")){
-			try(PreparedStatement ps = con.prepareStatement(sqlSelect)){
-				ps.setString(1, usuario);
+		try(Connection con = DriverManager.getConnection(URL_BBDD, USUARIO_BBDD, CONTRASEÑA_BBDD)){
+			try(PreparedStatement ps = con.prepareStatement(sqlSelectUsuario)){
+				//ps.setString(1, usuario);
 				try(ResultSet rs = ps.executeQuery()){
-					if(rs.next()) {
-						System.out.println("LLego hasta aqui");
+					//int i = 0;
+					for(int i = 1; rs.next(); i++) {
 						us = rs.getString("usuario");
-						contra = rs.getString("contraseña");
+						arrayUs.add(us);
+						System.out.println(arrayUs.get(0));
 					}
+					/*if(rs.next()) {
+						//System.out.println("LLego hasta aqui");
+						us = rs.getString("usuario");
+						System.out.println(arrayUs[i].toString());
+						arrayUs[i] = us;
+						
+						i++;
+					}*/
 				}catch(SQLException e) {
-					throw new RuntimeException("Error al pedir a la base de datos 2:", e);
+					//throw new RuntimeException("Error al pedir a la base de datos 2:", e);
 				}
 			}catch(SQLException e) {
-				throw new RuntimeException("Error al pedir a la base de datos", e);
+				//throw new RuntimeException("Error al pedir a la base de datos", e);
 			}
 		}catch(SQLException e) {
 			throw new RuntimeException("Error al conectar con la base de datos", e);
 		}
-		System.out.println(us.equals(null));
-		if(!us.equals(null)) {
-			System.out.println("Usuario: " + us + "\n" + "Contraseña: " + contra + "\n");
-			if(us.equals(usuario) && contra.equals(contraseña)) {
-				request.getSession().setAttribute("email", usuario);
-				response.sendRedirect("/WebPersonas/personas");
-			}else {
-				request.setAttribute("alertatexto", "El login no es correcto");
-				request.setAttribute("alertanivel", "danger");
-				request.setAttribute("email", usuario);
-				
-				request.getRequestDispatcher(LOGIN).forward(request, response);
+		boolean encontrado = false;
+		for(int j = 0; !encontrado || j < arrayUs.size(); j++) {
+			if(usuario.equals(arrayUs.get(j))) {
+				encontrado = true;
 			}
+		}
+		if(encontrado) {
+			try(Connection con = DriverManager.getConnection(URL_BBDD, USUARIO_BBDD, CONTRASEÑA_BBDD)){
+				try(PreparedStatement ps = con.prepareStatement(sqlSelectContraseña)){
+					ps.setString(1, us);
+					try(ResultSet rs = ps.executeQuery()){
+						contra = rs.getString("contraseña");
+					}
+				}
+			} catch (SQLException e) {
+				throw new RuntimeException("Error al conectar con la base de datos", e);
+			}
+		} else {
+			request.setAttribute("alertatexto", "El login no es correcto");
+			request.setAttribute("alertanivel", "danger");
+			request.setAttribute("email", usuario);
+			request.getRequestDispatcher(LOGIN).forward(request, response);
+		}
+		if(contra.equals(contraseña)) {
+			System.out.println("Usuario: " + us + "\n" + "Contraseña: " + contra + "\n");
+			request.getSession().setAttribute("email", usuario);
+			response.sendRedirect("/WebPersonas/personas");
 			
 		} else {
 			request.setAttribute("alertatexto", "El login no es correcto");
 			request.setAttribute("alertanivel", "danger");
-			//request.setAttribute("email", usuario);
-			
+			request.setAttribute("email", usuario);
 			request.getRequestDispatcher(LOGIN).forward(request, response);
 		//doGet(request, response);
 		}
